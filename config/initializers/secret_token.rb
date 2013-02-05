b8a50eb84a2643509b1ac1e9951726dc0f0e65b0
Murray Rowan <murrayrowan@yahoo.co.uk>
2013-02-05 11:06:53 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hacktools::Application.config.secret_token = 'edeffb313ad307ee426c84a46318a10ee0a67b1171bfb15b46395f3d18d2fbbeb0a2972d64e980579a7140c528218ca533d7ffbb6add5a3fd704e298c8fe6022'
